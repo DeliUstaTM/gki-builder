bbe7c2e9d368a40e8a30b88d5ba3d7ec8416ed23
Salih <91677076+DeliUstaTM@users.noreply.github.com>
2024-12-24 17:25:08 +0300

    revested ksunextci

--- build.sh
+++ build.sh
@@ -106,7 +106,7 @@ if [ "${USE_KSU_NEXT}" == "yes" ]; then
         KSU_NEXT_VERSION=$(git describe --abbrev=0 --tags)
         cd "$WORK_DIR"
 elif [ "${USE_KSU_NEXT_CI}" == "yes" ]; then
-        curl -LSs "https://raw.githubusercontent.com/rifsxd/KernelSU-Next/refs/heads/next-susfs-a12-5.10/kernel/setup.sh" | bash -
+        curl -LSs "https://raw.githubusercontent.com/rifsxd/KernelSU-Next/refs/heads/next/kernel/setup.sh" | bash -
         cd "$WORK_DIR/KernelSU-Next"
         KSU_NEXT_VERSION=$(git describe --abbrev=0 --tags)
         cd "$WORK_DIR"
