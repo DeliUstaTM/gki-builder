8e5cb2d525a08329452be53a375a10543baf0f54
gacorprjkt-admin <you@example.com>
2024-12-27 22:54:21 +0800

    cleanup uneeded packages
    * we are using hermetic toolchains

--- build.sh
+++ build.sh
@@ -50,8 +50,7 @@ fi
 
 ## Install needed packages
 sudo apt update -y
-sudo apt upgrade -y
-sudo apt install -y automake flex lzop bison gperf build-essential zip curl zlib1g-dev g++-multilib libxml2-utils bzip2 libbz2-dev libbz2-1.0 libghc-bzlib-dev squashfs-tools pngcrush schedtool dpkg-dev liblz4-tool make optipng maven libssl-dev pwgen libswitch-perl policycoreutils minicom libxml-sax-base-perl libxml-simple-perl bc libc6-dev-i386 lib32ncurses5-dev libx11-dev lib32z-dev libgl1-mesa-dev xsltproc unzip device-tree-compiler python2 rename libelf-dev dwarves rsync
+sudo apt install -y rsync bash git perl rsync sh tar
 
 ## Install Google's repo
 curl -so repo https://storage.googleapis.com/git-repo-downloads/repo
